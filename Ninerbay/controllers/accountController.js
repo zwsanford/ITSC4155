0f9c0915f8813d86facadc9a152c769284f0b975
Phil Vishnevsky <gadgetsphilop36@gmail.com>
2024-11-21 23:07:11 -0500

    fix unit tests for accountcontroller

--- Ninerbay/controllers/accountController.js
+++ Ninerbay/controllers/accountController.js
@@ -8,52 +8,49 @@ export const signupPage = (req, res) => {
     res.render('./account/signup');
 };
 
-export const create = (req, res, next) => {
-    let account = new Account(req.body);
-    if(account.email)
-        account.email = account.email.toLowerCase();
-    account.save()
-        .then(()=>{
-            req.flash('success', 'Registration Succeeded!');
-            next();
-            // res.redirect('/accounts/login');
-        })
-        .catch(err => {
-            if (err.name === 'ValidationError') {
-                req.flash('error', err.message);
-                err.status = 400;
-            }
-            next(err);
-        });
+export const create = async (req, res, next) => {
+    try {
+        let account = new Account(req.body);
+        if (account.email) account.email = account.email.toLowerCase();
+
+        await account.save();
+        req.flash('success', 'Registration Succeeded!');
+        next();
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            req.flash('error', err.message);
+            err.status = 400;
+        }
+        next(err);
+    }
 };
 
-export const login = (req, res, next) => {
-    let { username, password } = req.body;
+export const login = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
 
-    Account.findOne({ username: username })
-        .then(account => {
-            if (account) {
-                account.comparePassword(password)
-                    .then(isMatch => {
-                        if (isMatch) {
-                            req.session.user = account;
-                            req.flash('success', 'You have successfully logged in!');
-                            res.redirect('/');
-                        } else {
-                            req.flash('error', 'Invalid username or password');
-                            res.redirect('/accounts/login');
-                        }                        
-                })
-            }else{
-                req.flash('error', 'Wrong email address!');
-                res.redirect('/accounts/login');
-            }
-        })
-        .catch(err => next(err));
+        const account = await Account.findOne({ username });
+        if (!account) {
+            req.flash('error', 'Wrong email address!');
+            return res.redirect('/accounts/login');
+        }
+
+        const isMatch = await account.comparePassword(password);
+        if (isMatch) {
+            req.session.user = account; // Set session user
+            req.flash('success', 'You have successfully logged in!');
+            return res.redirect('/'); // Redirect to home
+        } else {
+            req.flash('error', 'Invalid username or password');
+            return res.redirect('/accounts/login');
+        }
+    } catch (err) {
+        next(err);
+    }
 };
 
 export const logout = (req, res, next) => {
-    req.session.destroy(err => {
+    req.session.destroy((err) => {
         if (err) {
             next(err);
             console.log('Error destroying session:', err);
@@ -62,6 +59,6 @@ export const logout = (req, res, next) => {
             res.redirect('/');
         }
     });
-}
+};
 
 export default { loginPage, signupPage, create, login, logout };
